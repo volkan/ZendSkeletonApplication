5770abc4f98302734cc23092234117c8c22ddad9
Volkan Altan <volkanaltan@gmail.com>
2012-09-16 14:34:20 +0300

    added zend developer tools

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,8 @@
 return array(
     'modules' => array(
         'Application',
-        'PhlyContact'
+        'PhlyContact',
+        'ZendDeveloperTools'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
