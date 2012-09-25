dc4f8e3f04561054f34bf1453030439786c2edf0
Volkan Altan <volkanaltan@gmail.com>
2012-09-25 23:19:00 +0300

    g

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
@@ -26,11 +19,11 @@ class IndexController extends AbstractActionController
     //view helper ve layout çalışmaz
     public function indexJsonAction()
     {
-    	$result = $this->getAlbumTable()->fetchAll();
+    	$result = $this->getAlbumTable()->findAll();
     	$serialize = serialize($result);
         $jsonModel = new JsonModel();
         $jsonModel->setVariable('result', $serialize);
-
+        //$jsonModel->setTerminal(false);
         return $jsonModel;
     }  
 
